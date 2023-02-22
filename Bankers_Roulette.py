415f5677d7a711a7f3293c64cbb4f441bb788c42
Jacob <57792656+wooyakob@users.noreply.github.com>
2023-02-22 13:54:26 -0800

    Update Bankers_Roulette.py
    Remove print

--- Bankers_Roulette.py
+++ Bankers_Roulette.py
@@ -3,6 +3,5 @@ names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 num_items = (len(names))
 random_choice = random.randint(0, num_items - 1)
-print(random_choice)
 person_who_will_pay = names[random_choice]
 print(person_who_will_pay + " is going to buy the meal today")
