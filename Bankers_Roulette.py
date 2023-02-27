f92f5b561301f4fe7aceb9c01989ca8b145bf4c8
Jacob <57792656+wooyakob@users.noreply.github.com>
2023-02-27 09:05:38 -0800

    Update Bankers_Roulette.py
    adding comments to better understand how the code is operating

--- Bankers_Roulette.py
+++ Bankers_Roulette.py
@@ -1,7 +1,16 @@
+#import random module
 import random
+#get user input for names of the group
 names_string = input("Give me everybody's names, separated by a comma. ")
+#split the string into individual names, for individual list items
 names = names_string.split(", ")
+#get the number of list items after being split
 num_items = (len(names))
+#select a random name by the number of the list item
 random_choice = random.randint(0, num_items - 1)
+#print the random choice / name
+print(random_choice)
+#define variable of the person who will pay 
 person_who_will_pay = names[random_choice]
+#print the newly defined variable, confirming the indivdual responsible for the entire bill
 print(person_who_will_pay + " is going to buy the meal today")
